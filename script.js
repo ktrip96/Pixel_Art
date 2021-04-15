60edce82ad411468926f978c74a2c57564e7ed15
ktrip96 <ktrip96@gmail.com>
2021-04-15 18:21:24 +0300

    Dimensions rdy

--- script.js
+++ script.js
@@ -22,19 +22,31 @@ function createGrid() {
   const width = getWidth()
   console.log({ height, width })
 
-  let grid = document.getElementById('grid')
   // each cell has 30px width and 30px height by default (this is my original assumption)
   // considering this assumption, I have to calculate how many cells
   // fit into the width and the height that were given by the user
 
-  grid.style.width = width
-  grid.style.height = height
-  //   grid.style.gridTemplateRows = 'repeat(20, 1fr)'
-  //   grid.style.gridTemplateColumns = 'repeat(20, 1fr)'
+  const gridRows = Math.floor(height / 30)
+  const gridColumns = Math.floor(width / 30)
+
+  // I used Math.floor because if the user gives 55px width, I want to display only one cell.
+  // However if the user gives 60px width, I will display 2 cells.
+  // This is my own assumption for this problem. I could solve it in many other ways.
+  // For example, I could change the cell size according to the user input for height and width.
+  // But I decided that its more beautiful if the cell remains fixed size.
+
+  let grid = document.getElementById('grid')
+  //   grid.style.width = width
+  //   grid.style.height = height
+  grid.style.gridTemplateRows = `repeat(${gridRows}, 1fr)`
+  grid.style.gridTemplateColumns = `repeat(${gridColumns}, 1fr)`
 
   console.log({ height, width })
+  console.log({ gridRows, gridColumns })
 
-  for (let i = 0; i < 100; i++) {
+  const totalCells = gridRows * gridColumns
+
+  for (let i = 0; i < totalCells; i++) {
     let cell = document.createElement('div')
     cell.className = 'cell'
     grid.appendChild(cell)
